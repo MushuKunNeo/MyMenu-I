f534a38338871dbeaa11370b26754800f4cf24b3
MushuKunNeo <manu.gregam@gmail.com>
2019-07-01 22:45:13 +0200

    Add All Menu with Side and Drinks

--- src/com/ocr/anthony/Main.java
+++ src/com/ocr/anthony/Main.java
@@ -3,7 +3,7 @@ package com.ocr.anthony;
 import java.util.Scanner;
 
 public class Main {
-
+3
     public static void main(String[] args) {
         Order order = new Order();
         order.runMenu();
