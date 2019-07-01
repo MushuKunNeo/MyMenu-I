6b0eac657bced82f286a114780ec1ff49939e428
MushuKunNeo <manu.gregam@gmail.com>
2019-07-01 21:02:09 +0200

    Add Side choose method

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -1,5 +1,7 @@
 package com.ocr.anthony;
 
+import java.util.Scanner;
+
 public class Order {
     /**
      * Display all available menus in the restaurant.
@@ -30,5 +32,55 @@ public class Order {
             System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
             break;
         }
+    }
+
+    /** Run Asking process for a Menu */
+    public void runMenu() {
+        this.displayAvailableMenu();
+        Scanner sc = new Scanner(System.in);
+        int nb = sc.nextInt();
+        this.displaySelectedMenu(nb);
+        /**
+        Scanner Side_Scan = new Scanner(System.in);
+        int No_Side = sc.nextInt();
+        this.displaySelectedSide(); */
+    }
+
+    /**
+     * Sélectionner un type de side, (et si choix parmit tout le rest.. très logique...)
+     * @param SideNo
+     * @param AllSideTrue
+     */
+    public void displaySelectedSide(int SideNo, boolean AllSideTrue) {
+        if (AllSideTrue) {
+        switch (SideNo) {
+            case 1:
+                System.out.println("Vous avez choisi comme accompagnement : légumes frais");
+                break;
+            case 2:
+                System.out.println("Vous avez choisi comme accompagnement : frites");
+                break;
+            case 3:
+                System.out.println("Vous avez choisi comme accompagnement : riz");
+                break;
+            default:
+                System.out.println("Vous n'avez pas choisi d'accompagnement parmi les choix proposés");
+                break;
+        }
+    } else {
+        switch (SideNo) {
+            case 1:
+                System.out.println("Vous avez choisi comme accompagnement : riz");
+                break;
+            case 2:
+                System.out.println("Vous avez choisi comme accompagnement : pas de riz");
+                break;
+            default:
+                System.out.println("Vous n'avez pas choisi d'accompagnement parmi les choix proposés");
+                break;
+        }
+    }
+
+
     }
 }
